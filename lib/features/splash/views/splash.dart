d66db99360c1464adc07d9365542650da29f5486
João Marchizio <jpmarchizio@gmail.com>
2022-09-30 16:44:27 -0300

    esboço de splash

---
+++ lib/features/splash/views/splash.dart
@@ -0,0 +1,32 @@
+import 'dart:async';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 3), () {
+      FirebaseAuth.instance.currentUser != null
+      //     ? Navigator.popAndPushNamed(context, '/tabs')
+      //     : Navigator.popAndPushNamed(context, '/login');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // backgroundColor: AppColor.purple,
+      body: Center(
+        // child: SvgPicture.asset('assets/splash.svg'),
+      ),
+    );
+  }
+}
