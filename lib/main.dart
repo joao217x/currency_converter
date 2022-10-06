2b8afb0af6c5e6c728bbeaf8da9e8245b3c66a25
João Marchizio <jpmarchizio@gmail.com>
2022-10-06 12:45:23 -0300

    providers em lista

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,19 @@ import 'package:currency_converter/features/login/controllers/login_store.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/single_child_widget.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
+  List<SingleChildWidget> providers = [
+    Provider<LoginStore>(create: (_) => LoginStore()),
+  ];
+
   runApp(
     MultiProvider(
-      providers: [
-        Provider<LoginStore>(create: (_) => LoginStore()),
-      ],
+      providers: providers,
       child: const CurrencyConverter(),
     ),
   );
